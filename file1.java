8d22f9d9a01d8c617ff55eadc285081ee06e7921
NailahPutri <nailahputri087@gmail.com>
2026-08-10 10:58:40 +0800

    Update file1.java

--- file1.java
+++ file1.java
@@ -1,5 +1,7 @@
 public class file1 {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("percobaan");
+        System.out.println("percobaan 3");
     }
 }
